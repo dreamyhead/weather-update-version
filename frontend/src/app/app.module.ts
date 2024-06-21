7d4ad371729fdfaa838d99e63706ac0fa7211a5a
HOME-PC\aleks <dr3amyh3ad@gmail.com>
2024-06-22 02:08:10 +0300

    working for adaptive

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,15 +1,18 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { APP_INITIALIZER, LOCALE_ID, NgModule } from '@angular/core';
+import { CommonModule, registerLocaleData } from '@angular/common';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { FeaturesModule } from './features/features.module';
 import { HttpClientModule } from '@angular/common/http';
 import { TranslationService } from './features/ui/shared/services/translation.service';
 import { FormsModule } from '@angular/forms';
+import localeRu from '@angular/common/locales/ru'
 
 export function initializeApp(translationService: TranslationService) {
   return () => translationService.loadTranslations('ru').toPromise();
 }
+
+registerLocaleData(localeRu);
 @NgModule({
   declarations: [
     AppComponent
@@ -21,6 +24,10 @@ export function initializeApp(translationService: TranslationService) {
     FeaturesModule,
   ],
   providers: [
+    { 
+      provide: LOCALE_ID, 
+      useValue: 'ru-RU' 
+    },
     {
       provide: APP_INITIALIZER,
       useFactory: initializeApp,
